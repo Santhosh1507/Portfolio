a7464556f9e22f4805ef153ee0f3d179ea9f0710
webdesign1507 <webdesign1507@gmail.com>
2024-04-27 21:18:40 +0530

    project are add

--- src/Component/Project/ProjectCard.jsx
+++ src/Component/Project/ProjectCard.jsx
@@ -3,20 +3,21 @@ import { getImageUrl } from "../../Images";
 import Styles from './Project1.module.css'
 
 export const ProjectCard = ({
-    project: { title, imageSrc, description, skills, demo, source },
+    project: { title, imageSrc, description, demo, source },
 }) => {
   return (
     <div className={Styles.container}>
          <img src={getImageUrl(imageSrc)} alt={title} className={Styles.image} />
          <h3 className={Styles.title}>{title}</h3>
-         {/* <p className={Styles.description}>{description}</p> */}
-         <ul className={Styles.skills}>
+         <p className={Styles.description}>{description}</p>
+         {/* <ul className={Styles.skills}>
          {skills.map((skill, id) => {
          return <li key={id} className={Styles.skill}>{skill}</li>;
          })}
-         </ul>
+         </ul> */}
          <div className={Styles.links}>
          <a href={demo} className={Styles.link}>Demo</a>
+         <a href={source} className={Styles.link}>Source</a>
          </div>                               
     </div>
   )
